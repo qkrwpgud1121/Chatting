88edb6410a06b2685f1c838692bd47df44d9bd77
jahyeang <qkrwpgud1121@gmail.com>
2025-02-10 16:45:51 +0900

    Initial Commit

---
+++ ChattingTests/ChattingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingTests.swift
+//  ChattingTests
+//
+//  Created by 박제형 on 2/10/25.
+//
+
+import Testing
+@testable import Chatting
+
+struct ChattingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
